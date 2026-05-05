d1862c908555b4a101c08551e2925b10126e3be7
jitendersingh205-1531 <jitendersingh205@gmail.com>
2026-05-05 19:18:31 +0530

    Update shop_app.py

--- shop_app.py
+++ shop_app.py
@@ -367,5 +367,11 @@ if st.session_state.page == "Stock":
 elif st.session_state.page == "Sales":
     sales_page()
 
+st.divider()
+st.subheader("🛠 Debug Tools")
+
+with open("shop.db", "rb") as f:
+    st.download_button("📥 Download Database", f, file_name="shop.db")
+
 elif st.session_state.page == "Reports":
     reports_page()
